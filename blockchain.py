eca502f9b1e6596dc99cf7ae5110e413e6a4007b
Shlok Nangia <SNangia@slb.com>
2018-09-16 19:03:51 +0530

    validate_chain function added

--- blockchain.py
+++ blockchain.py
@@ -25,3 +25,15 @@ class Blockchain:
     previous_block_hash = self.chain[len(self.chain)-1].hash
     new_block = Block(transactions, previous_block_hash)
     self.chain.append(new_block)
+
+  def validate_chain(self):
+    for i in range(1, len(self.chain)):
+      current = self.chain[i]
+      previous = self.chain[i-1]
+      if(current.hash != current.generate_hash()):
+        print("The current hash of the block does not equal the generated hash of the block.")
+        return False
+      if(current.previous_hash != previous.generate_hash()):
+        print("The previous block's hash does not equal the previous hash value stored in the current block.")
+        return False
+    return True
