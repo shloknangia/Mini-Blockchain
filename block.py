92f6b0cd30379c12a2215e3eac4662bbbc0ee3d7
Shlok Nangia <SNangia@slb.com>
2018-09-16 18:20:10 +0530

    add_block function added

--- block.py
+++ block.py
@@ -1,12 +1,11 @@
-from datetime import datetime
+import datetime
 from hashlib import sha256
 
 class Block:
-  def __init__(self, transactions, previous_hash, nonce = 0):
-    self.timestamp = datetime.now()
+  def __init__(self, transactions, previous_hash):
+    self.timestamp = datetime.datetime.now()
     self.transactions = transactions
     self.previous_hash = previous_hash
-    self.nonce = nonce
     self.hash = self.generate_hash()
     
   def print_block(self):
@@ -16,9 +15,6 @@ class Block:
     print("current hash:", self.generate_hash())
     
   def generate_hash(self):
-    # hash the blocks contents
-    block_contents = str(self.timestamp) + str(self.transactions) + str(self.nonce) + str(self.previous_hash)
+    block_contents = str(self.timestamp) + str(self.transactions) + str(self.previous_hash)
     block_hash = sha256(block_contents.encode())
-    
     return block_hash.hexdigest()
-    pass
