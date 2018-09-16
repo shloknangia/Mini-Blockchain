2dc95e949bf1d4c39cb198e1b15b7ae10fe2ad91
Shlok Nangia <SNangia@slb.com>
2018-09-16 19:45:43 +0530

    final commit

--- script.py
+++ script.py
@@ -1,22 +1,39 @@
-new_transactions = [{'amount': '30', 'sender':'alice', 'receiver':'bob'},
-               	{'amount': '55', 'sender':'bob', 'receiver':'alice'}]
+# new_transactions = [{'amount': '30', 'sender':'alice', 'receiver':'bob'},
+#                	{'amount': '55', 'sender':'bob', 'receiver':'alice'}]
 
-# import sha256
-from hashlib import sha256
-# sets the amount of leading zeros that must be found in the hash produced by the nonce
-difficulty = 2
-nonce = 0
+# # import sha256
+# from hashlib import sha256
+# # sets the amount of leading zeros that must be found in the hash produced by the nonce
+# difficulty = 2
+# nonce = 0
 
-# creating the proof 
-proof = sha256((str(nonce)+str(new_transactions)).encode()).hexdigest()
-# printing proof
-print(proof)
+# # creating the proof 
+# proof = sha256((str(nonce)+str(new_transactions)).encode()).hexdigest()
+# # printing proof
+# print(proof)
   
-# finding a proof that has 2 leading zeros
-while (proof[:2] != '0' * difficulty):
-  nonce += 1
-  proof = sha256((str(nonce) + str(new_transactions)).encode()).hexdigest()
+# # finding a proof that has 2 leading zeros
+# while (proof[:2] != '0' * difficulty):
+#   nonce += 1
+#   proof = sha256((str(nonce) + str(new_transactions)).encode()).hexdigest()
 
-# printing final proof that was found
-final_proof = proof
-print(final_proof)
+# # printing final proof that was found
+# final_proof = proof
+# print(final_proof)
+
+from blockchain import Blockchain
+
+block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
+block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
+block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
+fake_transactions = {"sender": "Bob", "receiver":"Cole, Alice", "amount":"25"}
+
+local_blockchain = Blockchain()
+local_blockchain.print_blocks()
+local_blockchain.add_block(block_one_transactions)
+local_blockchain.add_block(block_two_transactions)
+local_blockchain.add_block(block_three_transactions)
+local_blockchain.print_blocks()
+
+local_blockchain.chain[2].transactions = fake_transactions
+local_blockchain.validate_chain()
